ffa4cd7d3b400c93335bdfcaa166444ee4bcfe16
Erick Ayara <ayaraerick@gmail.com>
2023-11-08 21:43:47 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,20 @@
 /** @type {import('next').NextConfig} */
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-module.exports = {
-  compiler: {
-    styledComponents: true,
-  },
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'https://kim-rust.vercel.app/',
+      },
     ],
   },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    ) => {
-      plugins: [
-        config.module.generator.asset.publicPath = "/_next/",
-        new MiniCssExtractPlugin({
-          experimentalUseImportModule: false
-        }),
-      ]
-    return config
-  },
-};
+}
+
+
+
+module.exports = nextConfig
